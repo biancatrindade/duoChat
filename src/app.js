6d772d7e44db88940573c02e5789fa270514e199
Bianca Trindade <biancatrindade.mkt@gmail.com>
2022-12-14 21:16:05 -0300

    Documentation route

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ const learningLanguagesRoutes = require("./routes/learningLanguagesRoutes");
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
 
-    
-
 const app = express();
 
 app.use(express.json());
@@ -19,6 +17,6 @@ mongoose.connect();
 app.use("/duochat/users", usersRoutes);
 app.use("/duochat/countries", languagesCountriesRoutes);
 app.use("/duochat/languages", learningLanguagesRoutes);
-app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/my-documentation-route', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 module.exports = app;
